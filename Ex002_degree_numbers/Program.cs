ea64c633af6a602b82429a5e8a6318973293e92d
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-20 21:20:49 +0300

    Ex002_degree_numbers

---
+++ Ex002_degree_numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит возведение числа 
+// в заданную степень.
+
+Console.Write("Введите число: ");
+string UserNumber = Console.ReadLine() ?? "";
+int Number = int.Parse(UserNumber);
+
+Console.Write("Введите степень: ");
+string Degree = Console.ReadLine()?? "";
+int degree = int.Parse(Degree);
+
+double result = Math.Pow(Number, degree);
+
+Console.WriteLine($"Число {Number} в степени {Degree} -> {result}");
