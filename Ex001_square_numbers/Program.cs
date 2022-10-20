dd24e86ed6fd32be6e0bde7bcd4159d1503a70cc
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-20 21:01:35 +0300

    Ex001_square_numbers

--- Ex001_square_numbers/Program.cs
+++ Ex001_square_numbers/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает число
+// и выводит его квадрат
+
+Console.Write("Введите число: ");
+string UserNumber = Console.ReadLine() ?? "";
+int Number = int.Parse(UserNumber);
+
+int result = Number * Number;
+Console.WriteLine($"Квадрат числа {Number} -> {result}");
