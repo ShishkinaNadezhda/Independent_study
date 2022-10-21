97d6f82cad86849a457cf201d807f58acee47077
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-21 21:50:00 +0300

    Ex007_even_number

---
+++ Ex007_even_number/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить является ли число чётным.
+Console.Write("Введите число: ");
+string UsNum = Console.ReadLine() ?? "";
+double num = int.Parse(UsNum);
+double res = 0;
+
+res = num % 2;
+if(res == 0)
+{
+    Console.WriteLine($"Число {UsNum} является четным");
+}
+else
+{
+    Console.WriteLine($"Число {UsNum} не является четным");
+}
