c438bc78db1825778092e6874371cf20ec3fa891
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-21 20:32:46 +0300

    @Ex006_max_of_three_numbers

---
+++ Ex006_max_of_three_numbers/Program.cs
@@ -0,0 +1,43 @@
+﻿// Найти максимальное из трех чисел.
+Console.Write("Введите первое число: ");
+string UsNum1 = Console.ReadLine() ?? "";
+int num1 = int.Parse(UsNum1);
+
+Console.Write("Введите второе число: ");
+string UsNum2 = Console.ReadLine() ?? "";
+int num2 = int.Parse(UsNum2);
+
+Console.Write("Введите третье число: ");
+string UsNum3 = Console.ReadLine() ?? "";
+int num3 = int.Parse(UsNum3);
+
+int max = 0;
+
+if(num1 > num2)
+{
+    max = num1;
+}
+else
+{
+    max = num2;
+}
+
+if(num2 > num3)
+{
+    max = num2;
+}
+else
+{
+    max = num3;
+}
+
+if(num3 > num1)
+{
+    max = num3;
+}
+else
+{
+    max = num1;
+}
+
+Console.WriteLine($"Наибольшее число из трех - {max}");
