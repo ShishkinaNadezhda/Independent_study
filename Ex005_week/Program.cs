d1c057d2d4f7f7febac17f889bf0141fc4cdbb6f
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-21 20:16:00 +0300

    Ex005_week

---
+++ Ex005_week/Program.cs
@@ -0,0 +1,48 @@
+﻿// По заданному номеру дня недели вывести его название.
+Console.Write("Введите порядковый номер дня недели: ");
+string UserNumber = Console.ReadLine() ?? "";
+int number = int.Parse(UserNumber);
+
+if (number == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+
+if (number == 2)
+{
+    Console.WriteLine("Вторник");
+}
+
+if (number == 3)
+{
+    Console.WriteLine("Среда");
+}
+
+if (number == 4)
+{
+    Console.WriteLine("Четверг");
+}
+
+if (number == 5)
+{
+    Console.WriteLine("Пятница");
+}
+
+if (number == 6)
+{
+    Console.WriteLine("Суббота");
+}
+
+if (number == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+if (number > 7)
+{
+    Console.WriteLine("Ошибка ввода данных!");
+}
+
+if (number < 1)
+{
+    Console.WriteLine("Ошибка ввода данных!");
+}
