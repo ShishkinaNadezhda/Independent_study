4dc0475c0a75ad98a5c4c858db6a6aba471c3ae6
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-21 21:28:24 +0300

    Ex006_max-of-three_numbers

--- Ex006_max_of_three_numbers/Program.cs
+++ Ex006_max_of_three_numbers/Program.cs
@@ -12,32 +12,8 @@ string UsNum3 = Console.ReadLine() ?? "";
 int num3 = int.Parse(UsNum3);
 
 int max = 0;
-
-if(num1 > num2)
-{
-    max = num1;
-}
-else
-{
-    max = num2;
-}
-
-if(num2 > num3)
-{
-    max = num2;
-}
-else
-{
-    max = num3;
-}
-
-if(num3 > num1)
-{
-    max = num3;
-}
-else
-{
-    max = num1;
-}
+if (num1 > max) max = num1;
+if(num2 > max) max = num2;
+if (num3 > max) max = num3;
 
 Console.WriteLine($"Наибольшее число из трех - {max}");
