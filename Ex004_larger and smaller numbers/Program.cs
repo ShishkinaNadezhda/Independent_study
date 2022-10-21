11a7cd918d96a7d313cbe96bd1a194c8d936c720
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-21 19:52:32 +0300

    Ex004_larger_and_smaller_numbers

---
+++ Ex004_larger and smaller numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+Console.Write("Введите первое число ");
+string UserNumber1 = Console.ReadLine() ?? "";
+int Number1 = int.Parse(UserNumber1);
+
+Console.Write("Введите второе число ");
+string UserNumber2 = Console.ReadLine() ?? "";
+int Number2 = int.Parse(UserNumber2);
+
+if(Number1 > Number2)
+{
+    Console.Write($"Число {Number1} больше числа {Number2}");
+}
+else
+{
+    Console.Write($"Число {Number2} больше числа {Number1}");
+}
