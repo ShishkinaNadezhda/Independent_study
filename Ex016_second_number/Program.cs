b5fc2806ba18e09ee3ef9f32fcdd54816113935f
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-23 22:01:26 +0300

    Ex016_second_number

--- Ex016_second_number/Program.cs
+++ Ex016_second_number/Program.cs
@@ -8,17 +8,15 @@ Console.Write("Введите трехзначное число: ");
 string UsNum = Console.ReadLine() ?? "";
 int num = int.Parse(UsNum);
 
-if (num > 99)
-    Console.WriteLine("Ошибка ввода!");
-else if (num < 100)
-    Console.WriteLine("Ошибка ввода!");
-
 int num1 = 0;
 int num1_1 = 0;
 
 num1 = num / 10;
 num1_1 = num1 % 10;
 
-Console.WriteLine(num1_1);
+if (num > 100 && num < 999)
+    Console.WriteLine($"Вторая цифра числа: {num1_1}");
+else
+    Console.WriteLine("Ошибка ввода!");
 
 
