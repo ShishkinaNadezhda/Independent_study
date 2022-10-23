79539d5841adfcf0ddb8fb81ed640e311b4efd89
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-23 21:18:57 +0300

    Ex015_even_numbers

--- Ex015_even_numbers/Program.cs
+++ Ex015_even_numbers/Program.cs
@@ -5,22 +5,32 @@
 // 34, 5 -> не кратно, остаток 4
 // 16, 4 -> кратно
 
-Console.Write("Введите первое число: ");
-string UsNum1 = Console.ReadLine() ?? "";
+//Console.Write("Введите первое число: ");
+/*string UsNum1 = Console.ReadLine() ?? "";
 double num1 = int.Parse(UsNum1);
 
 Console.Write("Введите второе число: ");
 string UsNum2 = Console.ReadLine() ?? "";
-double num2 = int.Parse(UsNum2);
+double num2 = int.Parse(UsNum2);*/
 
-double res = num1 % num2;
-if (res > 0)
+int Enternumber()
 {
-    Console.WriteLine($"{num1} не кратно {num2}, остаток от деления {res}");
+    int number;
+    while(true)
+    {
+        Console.WriteLine("Введите число: ");
+        if (int.TryParse(Console.ReadLine(), out number))
+            break;
+        Console.WriteLine("Ошибка ввода!");
+    }
+    return number;
 }
-else
-{
+
+int num1 = Enternumber();
+int num2 = Enternumber();
+
+Console.Write($"{num1}, {num2} -> ");
+if(num1 % num2 ==0)
     Console.WriteLine($"{num1} кратно {num2}");
-}
-
-
+else
+    Console.WriteLine($"{num1} не кратно {num2}, остаток {num1 % num2}");
