d1f84cdd3e37fd3dd4284d21b398b298498bca4a
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-23 19:44:07 +0300

    Ex013_max_number

---
+++ Ex013_max_number/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выдает число из отрезка [10, 100]
+// и находит наибольшую цифру числа.
+
+int num = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число: {num}");
+
+int num1 = num / 10;
+int num2 = num % 10;
+
+if (num1 > num2)
+{
+    Console.WriteLine($"Первая цифра {num1} - максимальная");
+}
+else
+{
+    Console.WriteLine($"Вторая цифра {num2} - максимальная");
+}
