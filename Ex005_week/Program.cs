943a862d72184fd014456fa844ad658a6c7ca85b
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-23 12:31:30 +0300

    Ex003_week

--- Ex005_week/Program.cs
+++ Ex005_week/Program.cs
@@ -8,41 +8,41 @@ if (number == 1)
     Console.WriteLine("Понедельник");
 }
 
-if (number == 2)
+else if (number == 2)
 {
     Console.WriteLine("Вторник");
 }
 
-if (number == 3)
+else if (number == 3)
 {
     Console.WriteLine("Среда");
 }
 
-if (number == 4)
+else if (number == 4)
 {
     Console.WriteLine("Четверг");
 }
 
-if (number == 5)
+else if (number == 5)
 {
     Console.WriteLine("Пятница");
 }
 
-if (number == 6)
+else if (number == 6)
 {
     Console.WriteLine("Суббота");
 }
 
-if (number == 7)
+else if (number == 7)
 {
     Console.WriteLine("Воскресенье");
 }
-if (number > 7)
+else if (number > 7)
 {
     Console.WriteLine("Ошибка ввода данных!");
 }
 
-if (number < 1)
+else if (number < 1)
 {
     Console.WriteLine("Ошибка ввода данных!");
 }
