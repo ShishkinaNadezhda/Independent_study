011daebad4eefd84191dc6c08c796cf530e96206
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-23 11:35:02 +0300

    Ex002

---
+++ Ex002/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает два числа
+// и проверяет, является ли первое квадратом второго
+
+Console.Write("Введите первое число: ");
+string UsNum1 = Console.ReadLine() ?? "";
+int num1 = int.Parse(UsNum1);
+
+Console.Write("Введите второе число: ");
+string UsNum2 = Console.ReadLine() ?? "";
+int num2 = int.Parse(UsNum2);
+int square = num2*num2;
+
+if (square == num1)
+{
+    Console.WriteLine($"{UsNum1} является квадратом {UsNum2}");
+}
+else
+{
+    Console.WriteLine($"{UsNum1} не является квадратом {UsNum2}");
+}
