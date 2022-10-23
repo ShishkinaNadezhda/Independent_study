b7c78054851546ea902adcd196d5daeced7eb523
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-23 11:15:54 +0300

    Ex001

---
+++ Ex001/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает число
+// и выводит его квадрат.
+
+Console.Write("Введите число: ");
+string UsInp = Console.ReadLine() ?? "";
+int Num = int.Parse(UsInp);
+
+Console.WriteLine($"{UsInp} -> {Num * Num}");
