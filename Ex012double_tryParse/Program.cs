fd02f5c46818cd58c046299695fb578b261bf179
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-23 19:11:32 +0300

    Ex012_double_tryParse

--- Ex012double_tryParse/Program.cs
+++ Ex012double_tryParse/Program.cs
@@ -1,18 +1,11 @@
-﻿internal class Program
+﻿//Новые формулировки кода
+
+double varA;
+while (true)
 {
-    private static void Main(string[] args)
-    {
-        // Новые формулировки кода
-        static void Main(string[] args)
-        {
-            double varA;
-            while (true)
-            {
-                Console.Write("Введите число: ");
-                if (double.TryParse(Console.ReadLine(), out varA))
-                    break;
-                Console.WriteLine($"Ошибка ввода!");
-            }
-        }
-    }
+    Console.Write("Введите число: ");
+    if (double.TryParse(Console.ReadLine(), out varA))
+        break;
+    Console.WriteLine($"Ошибка ввода!");
 }
+Console.WriteLine($"A = {varA}");
