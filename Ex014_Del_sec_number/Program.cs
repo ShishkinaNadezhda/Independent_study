f8b86bfd6b5b6916a80bc38b4cd64c07701abd80
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-23 20:02:13 +0300

    Ex014_del_sec_number

---
+++ Ex014_Del_sec_number/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число
+// и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine($"{num} -> ");
+
+int num1 = num / 100;
+Console.WriteLine($"Первая цифра числа: {num1}");
+
+int num2 = num / 10;
+int num2_2 = num2 % 10;
+Console.WriteLine($"Вторая цифра числа: {num2_2}");
+
+int num3 = num % 10;
+Console.WriteLine($"Третья цифра числа: {num3}");
+
+int numres = num1 * 10 + num3;
+Console.WriteLine($"{num} -> {numres}");
+
