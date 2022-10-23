9280ad891f0553c8517ced25a2c43e73a657920b
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-23 11:54:28 +0300

    Ex003

---
+++ Ex003/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая по заданному номеру выдает название дня недели.
+
+Console.Write("Введите номер: ");
+string UsNum = Console.ReadLine() ?? "";
+int num = int.Parse(UsNum);
+
+if (num > 7)
+{
+    Console.WriteLine("Ошибка ввода!");
+}
+
+if (num == 0)
+{
+    Console.WriteLine("Ошибка ввода!");
+}
+
+if (num == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (num == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (num == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (num == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (num == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (num == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (num == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
