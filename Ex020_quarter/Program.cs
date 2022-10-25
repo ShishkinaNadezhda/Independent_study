abcad8d765037029c9ff5c51a67dd53c5a6bc599
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-25 20:30:23 +0300

    Ex020_quarter

---
+++ Ex020_quarter/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая по введенному номеру четверти будет показывать
+// возможный диапазон координат в этой четверти
+
+static void Quarter(int quarter)
+{
+    if (quarter == 1)
+        Console.WriteLine("x > 0, y > 0");
+
+    else if (quarter == 2)
+        Console.WriteLine("x < 0, y > 0");
+
+    else if (quarter == 3)
+        Console.WriteLine("x < 0, y < 0");
+
+    else if (quarter == 4)
+        Console.WriteLine("x > 0, y < 0");
+
+    else
+        Console.WriteLine("Некорректный ввод!");
+}
+
+int quarter;
+Console.WriteLine("Введите номер четверти: ");
+quarter = int.Parse(Console.ReadLine() ?? "");
+
+Quarter(quarter);
