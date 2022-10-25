8bd19f919bb909f0f53f3f9e4d970187dabb3211
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-25 21:27:00 +0300

    Ex022_degree

---
+++ Ex022_degree/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдает таблицу квадратов чисел от 1 до N
+
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write($"{N} -> ");
+PrintSquares(N);
+
+static void PrintSquares(int N)
+{
+    for(int i = 1; i < N; i++)
+    {
+        Console.Write($"{i * i}, ");
+    }
+    Console.Write($"{N * N} ");
+}
