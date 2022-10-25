eedf36fc0b20dec04d30ca9982468ce90a306965
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-25 19:19:15 +0300

    Ex019_sys_cords

---
+++ Ex019_Sys_cords/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (x, y)
+// и выдает номер четверти плоскости, к которой находится точка.
+// координаты не равняются нулю.
+
+Console.Write("Введите X: ");
+int X = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите Y: ");
+int Y = int.Parse(Console.ReadLine() ?? "");
+
+if (X > 0 && Y > 0)
+    Console.WriteLine("1");
+
+else if (X > 0 && Y < 0)
+    Console.WriteLine("2");
+
+else if (X < 0 && Y < 0)
+    Console.WriteLine("3");
+
+else if (X < 0 && Y > 0)
+    Console.WriteLine("4");
+
+else if (X == 0)
+    Console.WriteLine($"{X} неможет равняться нулю!");
+
+else if (Y == 0)
+    Console.WriteLine($"{Y} неможет равняться нулю!");
