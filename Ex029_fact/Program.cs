36bfd340802e61090b8ef0d795240b0bf6aaf54b
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-28 21:52:13 +0300

    Ex029_fact

---
+++ Ex029_fact/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдает произведение всех чисел от 1 до N
+// 4 -> 24;
+// 5 -> 120;
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+int a = 1;
+int fact = 1;
+
+while (a <= N)
+{
+    fact = a * fact;
+    a++;
+}
+Console.WriteLine($"{fact}");
+
