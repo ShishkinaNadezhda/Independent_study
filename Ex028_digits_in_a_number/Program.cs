77d11071554fff95a1674c53e87010fe50ffa53b
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-28 20:11:19 +0300

    Ex028_digits_in_a_number

---
+++ Ex028_digits_in_a_number/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число,
+// и выдает количество цифр в этом числе.
+// 456 -> 3
+// 78 -> 2
+
+Console.Clear();
+Console.Write("Введите число: ");
+string number = Console.ReadLine() ?? "";
+int len = number.Length;
+Console.Write($"{number} -> {len}");
+
