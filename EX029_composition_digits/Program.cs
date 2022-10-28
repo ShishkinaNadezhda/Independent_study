53a8407de918c40a80175a52967408f6728ef88e
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-28 21:18:14 +0300

    Ex029_composition_digits

---
+++ EX029_composition_digits/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдает произведение всех чисел от 1 до N
+// 4 -> 24;
+// 5 -> 120;
+
+int num = GetUserFromNumber("Введите целое число A: ", "Ошибка ввода!");
+int compozNumbers = GetCompozNumbers(num);
+Console.WriteLine($"{num} -> {compozNumbers}");
+
+int GetUserFromNumber(string message, string errormessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool IsCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (IsCorrect)
+        {
+            return userNumber;
+        }
+        Console.WriteLine(errormessage);
+    }
+}
+
+int GetCompozNumbers(int number)
+{
+    int compoz = 1;
+    while (number > 0)
+    {
+        compoz*=number;
+        number--;
+    }
+    return compoz;
+}
+
