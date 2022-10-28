b4cfe519ddfbb2a445e384ab640ccd113aea235e
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-28 19:25:31 +0300

    Ex026_matrix

--- Ex026_matrix/Program.cs
+++ Ex026_matrix/Program.cs
@@ -1,11 +1,30 @@
 ﻿// двумернй массив
 
-int[,] matrix = new int [3, 4];
-for (int i = 0; i < 3; i++)
+void PrintArray(int[,] matr)
 {
-    for(int j = 0; j < 4; j++)
+    for (int i = 0; i < matr.GetLength(0); i++)
     {
-        Console.Write($"{matrix[i, j]} ");
-    }
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
     Console.WriteLine();
+    }
 }
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+int[,] matr = new int [3, 4];
+PrintArray(matr);
+FillArray(matr);
+Console.WriteLine("");
+PrintArray(matr);
