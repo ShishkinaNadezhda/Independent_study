23a903d3bad7837df9531bce96232b828e5a42f4
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-29 15:18:44 +0300

    Ex030_random_8

---
+++ Ex030_random_8/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит массив из 1 и 0 в случайном порядке.
+// [0, 1, 1, 0, 1, 0, 0, 0]
+
+
+int[] array = GetNumber(8);
+
+for (int i = 0; i < 8; i++)
+{
+    Console.Write($"{array[i] + " "} ");
+}
+
+
+static int[] GetNumber(int size)
+{
+    int [] array = new int[size];
+    for (int i = 0; i < 8; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+
+    }
+    return array;
+}
